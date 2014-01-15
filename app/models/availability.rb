412bbd2cebec6bef3fb16affb1f18fbcd7d35d48
yhengkang <yhengkang@gmail.com>
2014-01-15 14:16:01 -0800

    guest spaces for availability must be > 0 now

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,6 +1,8 @@
 class Availability < ActiveRecord::Base
   attr_accessible :activity_id, :date, :start_time, :end_time, :price, :spaces
   validates :activity_id, :date, :start_time, :end_time, :price, :spaces, :presence => true
+  validates :spaces, :numericality => {:greater_than => 0}
+  
   #needs custom validation for overlapping of times
  	#what if the same classes is offered by the same vendor, at overlapping times, by different instructors?
 
