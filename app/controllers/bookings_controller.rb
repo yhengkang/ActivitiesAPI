7520f7eb00f50f50ce08d4b1572f8bc1b49dd1aa
yhengkang <yhengkang@gmail.com>
2014-01-15 14:07:02 -0800

    moves validation for spaces in availability to booking model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,35 +1,18 @@
 class BookingsController < ApplicationController
 
 	#should move the availability space updates to model layer of bookings
-	
+
 	def create
 		@booking = Booking.new(params[:booking])
-		availability_id = params[:booking][:availability_id]
-		begin
-			@booking.transaction do
-				@booking.save
-				#modify availability spaces left
-				@availability = Availability.find(availability_id)
-				@availability.update_attribute(:spaces, (@availability.spaces - @booking.guests) )
-			end
+		if @booking.save
 			render :json => @booking
-		rescue ActiveRecord::RecordInvalid => invalid
+		else
 			render :json => @booking.errors.full_messages
 		end
 	end
 
 	def destroy
-		@booking = Booking.find(params[:id])
-		availability_id = @booking.availability_id
-		begin
-			@booking.transaction do
-				@availability = Availability.find(availability_id)
-				@availability.update_attribute(:spaces, (@availability.spaces + @booking.guests) )
-				@booking.destroy
-			end
-			render :json => {:head => :ok}
-		rescue ActiveRecord::RecordInvalid => invalid
-			render :json => {:status => :unprocessable_entity}
-		end
+		Booking.find(params[:id]).destroy
+		render :json => {:head => :ok}
 	end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,23 @@ class Booking < ActiveRecord::Base
   attr_accessible :availability_id, :guests, :user_id
   validates :availability_id, :guests, :presence => true
 
- 	#custom validation to check if the availability has enough spaces left
+ 	validate :availability_must_have_space
+
+ 	before_destroy :add_space_back
+
+ 	def availability_must_have_space
+ 		availability = Availability.find(self.availability_id)
+ 		if availability.spaces < self.guests
+ 			errors.add(:guests, "Too many guests for spots remaining")
+ 		else
+ 			#update spaces lef if valid
+ 			availability.update_attribute(:spaces, (availability.spaces - self.guests) )
+ 		end
+ 	end
+
+ 	def add_space_back
+ 		availability = Availability.find(self.availability_id)
+ 		availability.update_attribute(:spaces, (availability.spaces + self.guests) )
+ 	end
+
 end
