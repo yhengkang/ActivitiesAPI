3eff6bb6aa13e25c0b507db3001aa7e075660428
yhengkang <yhengkang@gmail.com>
2014-01-14 19:45:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeekCodingChallenge::Application.config.secret_token = '337b43231429655463a4436867b9e3485252fb4125205fa7c652b74ae5530ef94f11b97892ac0ee3e46b4106033720bb8c3138d500157333639ca206850fd8e0'
